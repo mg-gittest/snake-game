5ba14ea8f8d3930b35ab726e9a7bddf9b94fe64c
Mark Germain <markgermain@btinternet.com>
2015-09-11 09:41:14 +0100

    newly generated project, with some xml layout tweaks to make me happy

---
+++ app/src/main/java/consulting/germain/snakegame/FieldActivityFragment.java
@@ -0,0 +1,22 @@
+package consulting.germain.snakegame;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class FieldActivityFragment extends Fragment {
+
+    public FieldActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_field, container, false);
+    }
+}
